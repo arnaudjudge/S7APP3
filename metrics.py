e52ebcf25e6a6889d1a81a0fbeaf7597e29122ed
ArnaudJudge <arnaud.judge@usherbrooke.ca>
2022-03-16 13:29:53 -0400

    add edit dist from lab

--- metrics.py
+++ metrics.py
@@ -5,10 +5,29 @@ import numpy as np
 
 def edit_distance(x,y):
     # Calcul de la distance d'édition
-    
-    # À compléter
-    
-    return None
+
+    a = ['#'] + x # padding
+    b = ['#'] + y
+    l_a = len(a)
+    l_b = len(b)
+
+    mat = np.zeros((l_a, l_b))
+    for i in range(l_a):
+        mat[i, 0] = i
+    for j in range(l_b):
+        mat[0, j] = j
+
+    for i in range(1, l_a):
+        for j in range(1, l_b):
+            if a[i] == b[j]:
+                cost = 0
+            else:
+                cost = 1
+
+            mat[i, j] = min(mat[i-1, j]+1,
+                            mat[i, j-1]+1,
+                            mat[i-1, j-1]+cost)
+    return mat[l_a-1, l_b-1]
 
 def confusion_matrix(true, pred, ignore=[]):
     # Calcul de la matrice de confusion
