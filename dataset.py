66c9f8252eb133e3e413d0950879c03abf301872
ArnaudJudge <arnaud.judge@usherbrooke.ca>
2022-03-16 11:34:01 -0400

    dataset

--- dataset.py
+++ dataset.py
@@ -17,6 +17,9 @@ class HandwrittenWords(Dataset):
         self.start_symbol   = start_symbol = '<sos>'
         self.stop_symbol    = stop_symbol = '<eos>'
 
+        self.symb_dict = {self.pad_symbol: 2, self.start_symbol:0, self.stop_symbol:1}
+        counter_symb = 3
+
         self.raw_data = dict()
         with open(filename, 'rb') as fp:
             self.raw_data = pickle.load(fp)
@@ -24,23 +27,42 @@ class HandwrittenWords(Dataset):
         # Extraction des symboles
         self.data = dict()
         for i in range(len(self.raw_data)):
-            self.data[i] = (self.raw_data[i][0], torch.stack((torch.tensor(self.raw_data[i][1][0]), torch.tensor(self.raw_data[i][1][1]))))
+            #creer le dict avec tout les symboles
+            label = list(self.raw_data[i][0])
+            for symb in label:
+                if symb not in self.symb_dict:
+                    self.symb_dict[symb] = counter_symb
+                    counter_symb += 1
+            coords = torch.stack((torch.tensor(self.raw_data[i][1][0]), torch.tensor(self.raw_data[i][1][1])))
+            # coords = (self.raw_data[i][1][0], self.raw_data[i][1][1])
+            self.data[i] = (label, coords)
 
-
-        print(1)
         # Ajout du padding aux séquences
-        # À compléter
-        
+        self.max_len = dict()
+        self.max_len['labels'] = len(max(self.data[0], key=len)) + 1
+        print(self.max_len['labels'])
+        print(self.data[0])
+        self.max_len['coords'] = max(self.data[1][0].shape, key=np.size) + 1
+        print(self.max_len['coords'])
+        for i in range(len(self.data[0])):
+            self.data[0] = (self.data[0][i] + [self.stop_symbol] + [self.pad_symbol for _ in range(self.max_len['labels'] - len(self.data[0][i]) - 1)], self.data[1][i])
+
+
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx][0], self.data[idx][1]
+        label = self.data[idx][0]
+        symb_label = [self.symb_dict[i] for i in label]
+        return torch.tensor(symb_label), self.data[idx][1]
 
     def visualisation(self, idx):
         # Visualisation des échantillons
-        # À compléter (optionel)
-        pass
+        item = self.__getitem__(idx)
+        plt.figure()
+        plt.plot(item[1][0], item[1][1], '-bo')
+        plt.title(item[0])
+        plt.show()
         
 
 if __name__ == "__main__":
